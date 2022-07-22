f87e6ac6c271d67034cc47e167698cd3a8270d1f
Vladimir Ku <aquaform@yandex.ru>
2022-07-22 15:26:56 +0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,39 @@
+import React, { useState, useLayoutEffect } from "react";
+import { render } from "react-dom";
+import mapboxgl from "mapbox-gl";
+import "./styles.css";
+
+function App() {
+  mapboxgl.accessToken = "ЗАМЕНИТЬ_НА_СВОЙ_ТОКЕН";
+
+  useLayoutEffect(() => {
+    const map = new mapboxgl.Map({
+      container: "map",
+      style: "mapbox://styles/mapbox/dark-v10",
+      center: [37.610641, 55.761994],
+      zoom: 10
+    });
+  }, []);
+
+  const stores = {
+    km20: [37.610641, 55.761994],
+    belief: [37.601152, 55.733396],
+    brandshop: [37.616812, 55.767839]
+  };
+
+  return (
+    <>
+      <div className="map-overlay">
+        <h3>Выберите магазин: </h3>
+        <select>
+          <option value="km20">KM20</option>
+          <option value="belief">BELIEF</option>
+          <option value="brandshop">BRANDSHOP</option>
+        </select>
+      </div>
+      <div id="map"></div>
+    </>
+  );
+}
+
+render(<App />, document.querySelector("#root"));
